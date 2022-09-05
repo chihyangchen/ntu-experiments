4b0d1c1690dee52f8a58b05db158b5ecaa4cdee7
chihyangchen <chihyang78@gmail.com>
2022-09-05 09:40:49 +0000

    revise the modem-info to use fixed at path

--- modem-utilities/modem-info.sh
+++ modem-utilities/modem-info.sh
@@ -10,32 +10,52 @@
 helpFunction()
 {
     echo ""
-    echo "Usage: $0 -d [/dev/ttyUABX] {-t [delay]}"
-    echo "/dev/ttyUSBX: X is the num of device"
+    echo "Usage: $0 -i [interface] {-t [delay]}"
+    echo "interface: network interface"
     echo "sleep time: wait time of each information capture"
     exit 1 # Exit script after printing help
 }
 
-while getopts "d:t:" opt
+while getopts "i:t:" opt
 do
     case "$opt" in
-        d ) dev="$OPTARG" ;;
+        i ) interface="$OPTARG" ;;
         t ) delay="$OPTARG" ;;
         ? ) helpFunction ;;
     esac
 done
 
-if [ -z "$dev" ] 
+if [ -z "$interface" ] 
 then
     echo "missing argument"
     helpFunction
 fi
 
+for i in $@ ;do
+    case "$i" in
+    wwan0)
+        DEV_AT_PATH="/dev/serial/by-id/usb-Quectel_RM500Q-GL_7444b2b7-if03-port0"
+        ;;
+    quectel0)
+        DEV_AT_PATH="/dev/serial/by-id/usb-Quectel_RM500Q-GL_76857c8-if03-port0"
+        ;;
+    quectel1)
+        DEV_AT_PATH="/dev/serial/by-id/usb-Quectel_RM500Q-GL_bc4587d-if03-port0"
+        ;;
+    quectel2)
+        DEV_AT_PATH="/dev/serial/by-id/usb-Quectel_RM500Q-GL_5881b62f-if03-port0"
+        ;;
+    quectel3)
+        DEV_AT_PATH="/dev/serial/by-id/usb-Quectel_RM500Q-GL_32b2bdb2-if03-port0"
+        ;;
+    esac
+done
+
 capture()
 {
     #FIXME: add the current timestamp
-    mxat -d $dev -c at+qeng=\"servingcell\" -t 3000
-    mxat -d $dev -c at+qeng=\"neighbourcell\" -t 3000
+    mxat -d $DEV_AT_PATH -c at+qeng=\"servingcell\" -t 3000
+    mxat -d $DEV_AT_PATH -c at+qeng=\"neighbourcell\" -t 3000
        
 }
 
