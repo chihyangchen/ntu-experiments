c35947e48e0a738724d957472715d831c7bad73f
shengru55688 <riven88926@gmail.com>
2023-03-04 16:51:22 +0800

    3/4

---
+++ sheng-ru/experiment/iperf/iperf_client.py
@@ -0,0 +1,30 @@
+import subprocess
+import time
+import datetime as dt
+import os
+import signal
+
+server_ip = '140.112.20.183'
+port = 3250
+packet_length = 500 # bytes
+band_width = '200K' # bits/sec
+experiment_time = 3600
+
+now = dt.datetime.today()
+t = '-'.join([str(x) for x in[ now.year%100, now.month, now.day, now.hour, now.minute]])
+save_file = f"/home/wmnlab/Data/{t}_client.pcap"
+tcpproc = subprocess.Popen([f"tcpdump -i any port {port} -w {save_file}"],
+                            shell=True, preexec_fn=os.setsid)
+time.sleep(1)
+
+iperfproc = subprocess.Popen([f"iperf3 -c {server_ip} -p {port} -b {band_width} -l {packet_length}"\
+                              f"--bidir -t {experiment_time}"], 
+                             shell=True, preexec_fn=os.setsid)
+
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    os.killpg(os.getpgid(tcpproc.pid), signal.SIGTERM)
+    os.killpg(os.getpgid(iperfproc.pid), signal.SIGTERM)
