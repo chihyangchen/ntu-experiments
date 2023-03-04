332e7055db996b5109bfe57ec71e084f32f130f3
shengru55688 <riven88926@gmail.com>
2023-03-04 17:42:44 +0800

    3/4 2

--- sheng-ru/experiment/iperf/iperf_server.py
+++ sheng-ru/experiment/iperf/iperf_server.py
@@ -4,26 +4,19 @@ import datetime as dt
 import os
 import signal
 
-server_ip = '140.112.20.183'
 port1 = 3250
 port2 = 3251
-packet_length = 500 # bytes
-band_width = '200K' # bits/sec
-experiment_time = 3600 # sec
 
 now = dt.datetime.today()
 t = '-'.join([str(x) for x in[ now.year%100, now.month, now.day, now.hour, now.minute]])
-save_file = f"/home/wmnlab/Data/{t}_client.pcap"
+save_file = "/home/wmnlab"
+save_file = f"/home/wmnlab/D/sheng-ru/Data/{t}_server.pcap"
 tcpproc = subprocess.Popen([f"tcpdump -i any port {port1} or port {port2} -w {save_file}"],
                             shell=True, preexec_fn=os.setsid)
-
 time.sleep(1)
 
-iperfproc1 = subprocess.Popen([f"iperf3 -c {server_ip} -p {port1} -b {band_width} -l {packet_length} -t {experiment_time}"], 
-                             shell=True, preexec_fn=os.setsid)
-
-iperfproc2 = subprocess.Popen([f"iperf3 -c {server_ip} -p {port2} -b {band_width} -l {packet_length} -t {experiment_time}"], 
-                             shell=True, preexec_fn=os.setsid)
+iperfproc1 = subprocess.Popen([f"iperf3 -s -B 0.0.0.0 -p {port1}"], shell=True, preexec_fn=os.setsid)
+iperfproc2 = subprocess.Popen([f"iperf3 -s -B 0.0.0.0 -p {port2}"], shell=True, preexec_fn=os.setsid)
 
 try:
     while True:
