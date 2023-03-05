5d22286688a1789e4b7513baa243c820627c86d3
shengru55688 <riven88926@gmail.com>
2023-03-05 17:13:17 +0800

    3/5

--- sheng-ru/post_processing/pcap_to_csv.py
+++ sheng-ru/post_processing/pcap_to_csv.py
@@ -2,12 +2,13 @@ import subprocess
 import os
 import sys
 import time
-def pcap_to_csv(infilepath, outfilepath):
 
-    s = f"tshark -r {infilepath} -T fields -e frame.number -e frame.time -e ip.src -e ip.dst -e _ws.col.Protocol"\
-        "-e frame.len -e tcp.analysis.acks_frame -e tcp.seq_raw -e tcp.len -e tcp.analysis.ack_rtt -e tcp.srcport"\
-        "-e tcp.dstport -e tcp.analysis.bytes_in_flight -e tcp.analysis.retransmission -e tcp.analysis.fast_retransmission"\
-        f"-e tcp.analysis.out_of_order -e tcp.payload -E header=y -E separator=@ >{outfilepath}"
+def pcap_to_csv(infilepath, outfilepath):
+    # TCP
+    s = f"tshark -r {infilepath} -T fields -e frame.number -e frame.time -e ip.src -e ip.dst -e _ws.col.Protocol "\
+        "-e frame.len -e tcp.analysis.acks_frame -e tcp.seq_raw -e tcp.len -e tcp.analysis.ack_rtt -e tcp.srcport "\
+        "-e tcp.dstport -e tcp.analysis.bytes_in_flight -e tcp.analysis.retransmission -e tcp.analysis.fast_retransmission "\
+        f"-e tcp.analysis.out_of_order -E header=y -E separator=@ >{outfilepath}"
     
 #     s = "tshark -r %s -T\
 # fields -e frame.number -e frame.time -e ip.src -e ip.dst\
@@ -16,9 +17,6 @@ def pcap_to_csv(infilepath, outfilepath):
     print(s)
     subprocess.Popen([s], shell=True)
     time.sleep(20)
-
-
-
 
 if os.path.isdir(sys.argv[1]):
 
