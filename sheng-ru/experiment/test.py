899941616c8fbe66560aad365ba51ef38575e77a
shengru55688 <riven88926@gmail.com>
2023-03-06 20:33:54 +0800

    3/6 3

--- sheng-ru/experiment/test.py
+++
@@ -1,6 +0,0 @@
-import os
-import subprocess
-import threading
-
-subprocess.Popen(['sudo tcpdump port 3250 -w /home/wmnlab/123.pcap'],  shell=True, preexec_fn = os.setpgrp)
-os.system('iperf3 -s -B 0.0.0.0 -p 3250')
