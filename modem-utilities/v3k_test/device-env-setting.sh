e93c8c6d973c72d2f41fca4b41820402e4931950
chihyangchen <chihyang78@gmail.com>
2024-04-25 11:02:42 +0800

    delpoyment setting for Metro

---
+++ modem-utilities/v3k_test/device-env-setting.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+SUDO=sudo
+
+
+helpFunction()
+{
+    echo ""
+    echo "Usage: $0 -d [Metro#xxx] "
+    echo "ENTER 1, 2, 3 or 4"
+    exit 1 # Exit script after printing help
+}
+while getopts "d:" opt
+do
+    case "$opt" in
+        d ) DEV="$OPTARG" ;;
+        ? ) helpFunction ;;
+    esac
+done
+if [ -z "$DEV" ]
+then
+    echo "missing argument"
+    helpFunction
+fi
+
+
+v3k_test_dir=`(pwd)`
+echo "$v3k_test_dir"
+
+NAME="Metro#"
+DEV="${NAME}${DEV}"
+echo "$DEV"
+
+
+cd ..
+utility_dir=`(pwd)`
+echo "$utility_dir"
+to_be_replaced_dir="/home/moxa/young/ntu-experiments/modem-utilities"
+
+sed -i 's#'${to_be_replaced_dir}'#'${utility_dir}'#g' PATH_for_NTU_exp
+
+
+cd
+home_dir=`(pwd)`
+touch "${DEV}"
+
+cd "$utility_dir"
+${SUDO} cp PATH_for_NTU_exp /usr/local/bin
+
+cd "$v3k_test_dir"
+${SUDO} cp v3000_TMetro.sh /usr/local/bin
+${SUDO} cp v3000_TMetro.service /etc/systemd/system
+${SUDO} systemctl enable v3000_TMetro.service
+
