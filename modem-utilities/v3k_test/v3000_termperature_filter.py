c4586cd22c1dffae6021888cef3708856287f07a
chihyangchen <chihyang78@gmail.com>
2024-04-29 14:05:36 +0800

    v3000 system log function

--- modem-utilities/v3k_test/v3000_termperature_filter.py
+++ modem-utilities/v3k_test/v3000_termperature_filter.py
@@ -4,16 +4,23 @@ import os
 import sys
 import pandas as pd
 import numpy
+target_module_1='m21'
+target_module_2='m22'
 
 target=["time","qfe_wtr_pa0","qfe_wtr_pa1","qfe_wtr_pa2","qfe_wtr_pa3"]
 target_nvme=["Sensor 1","Sensor 2","Sensor 3","Sensor 4"]
 target_core=["Core 0","Core 1","Core 2","Core 3"]
 target_sata=["drivetemp","temp1"]
 
-l_qfe_wtr_pa0=[]
-l_qfe_wtr_pa1=[]
-l_qfe_wtr_pa2=[]
-l_qfe_wtr_pa3=[]
+l_qfe_wtr_pa0_1=[]
+l_qfe_wtr_pa1_1=[]
+l_qfe_wtr_pa2_1=[]
+l_qfe_wtr_pa3_1=[]
+
+l_qfe_wtr_pa0_2=[]
+l_qfe_wtr_pa1_2=[]
+l_qfe_wtr_pa2_2=[]
+l_qfe_wtr_pa3_2=[]
 
 l_sensor_nvme_1=[]
 l_sensor_nvme_2=[]
@@ -30,18 +37,11 @@ l_sensor_sata_1=[]
 m_export=[]
 
 sata_en=False
-
-#def check_content(input):
-#	for item in target:
-#		if(item in input[0]):
-#			return input[1]
-
+module1_en=False
+module2_en=False
 
 
 if __name__ == "__main__":
-#	if(len(sys.argv) < 3):
-#		print("enter file_in_path file_out_path")
-#		sys.exit()
 	file_in = sys.argv[1]
 #	file_out = sys.argv[2]
 
@@ -49,8 +49,15 @@ if __name__ == "__main__":
 		lines = f.readlines()
 #		print (lines)
 	for word in lines:
+		dev=word.split(',')
+		if dev[-1][:-1] == target_module_1:
+			module1_en = True
+			module2_en = False
+		elif dev[-1][:-1] == target_module_2:
+			module1_en = False
+			module2_en = True 
 		word=word.split('"')
-#		print(word)
+#		print(dev)
 		if (len(word) < 2):
 			temp_system=word[0]
 			tmp_for_sata = temp_system.split("-")
@@ -110,14 +117,26 @@ if __name__ == "__main__":
 				sata_en = False
 		elif (len(word) > 2 and word[1] == target[1]):
 #			print(word[3])
-			l_qfe_wtr_pa0.append(word[3])
+			if module1_en:
+				l_qfe_wtr_pa0_1.append(word[3])
+			elif module2_en:
+				l_qfe_wtr_pa0_2.append(word[3])
 		elif (len(word) > 2 and word[1] == target[2]):
-			l_qfe_wtr_pa1.append(word[3])
+			if module1_en:
+				l_qfe_wtr_pa1_1.append(word[3])
+			elif module2_en:
+				l_qfe_wtr_pa1_2.append(word[3])
 		elif (len(word) > 2 and word[1] == target[3]):
-			l_qfe_wtr_pa2.append(word[3])
+			if module1_en:
+				l_qfe_wtr_pa2_1.append(word[3])
+			elif module2_en:
+				l_qfe_wtr_pa2_2.append(word[3])
 		elif (len(word) > 2 and word[1] == target[4]):
-			l_qfe_wtr_pa3.append(word[3])
-#	print(l_qfe_wtr_pa0)
+			if module1_en:
+				l_qfe_wtr_pa3_1.append(word[3])
+			elif module2_en:
+				l_qfe_wtr_pa3_2.append(word[3])
+#	print(l_qfe_wtr_pa0_2)
 #	print(l_qfe_wtr_pa1)
 #	print(l_qfe_wtr_pa2)
 #	print(l_qfe_wtr_pa3)
@@ -131,12 +150,19 @@ if __name__ == "__main__":
 #	print(l_sensor_nvme_3)
 #	print(l_sensor_nvme_4)
 
-	print(l_sensor_sata_1)
-	m_export.append(l_qfe_wtr_pa0)
-	m_export.append(l_qfe_wtr_pa1)
-	m_export.append(l_qfe_wtr_pa2)
-	m_export.append(l_qfe_wtr_pa3)
+#	print(l_sensor_sata_1)
+
+	m_export.append(l_qfe_wtr_pa0_1)
+	m_export.append(l_qfe_wtr_pa1_1)
+	m_export.append(l_qfe_wtr_pa2_1)
+	m_export.append(l_qfe_wtr_pa3_1)
 	
+	m_export.append(l_qfe_wtr_pa0_2)
+	m_export.append(l_qfe_wtr_pa1_2)
+	m_export.append(l_qfe_wtr_pa2_2)
+	m_export.append(l_qfe_wtr_pa3_2)
+	
+	m_export.append(l_sensor_core_0)
 	m_export.append(l_sensor_core_0)
 	m_export.append(l_sensor_core_1)
 	m_export.append(l_sensor_core_2)
@@ -154,3 +180,4 @@ if __name__ == "__main__":
 	print(numpy.transpose(m_export))
 	df = pd.DataFrame(numpy.transpose(m_export))
 	df.to_csv('output.csv', index=False, header=False )
+
